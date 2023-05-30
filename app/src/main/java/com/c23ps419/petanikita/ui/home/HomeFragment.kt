8cddd630a94fc7e0354a3a842bec83e97f2ae799
aleirsyad <irsyadabqori46@gmail.com>
2023-05-30 23:10:32 +0700

    create fragment for home, scan and profile

---
+++ app/src/main/java/com/c23ps419/petanikita/ui/home/HomeFragment.kt
@@ -0,0 +1,29 @@
+package com.c23ps419.petanikita.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.c23ps419.petanikita.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+
+    private var _fragmentHomeBinding: FragmentHomeBinding? = null
+    private val homeBinding get() = _fragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _fragmentHomeBinding = FragmentHomeBinding.inflate(inflater, container, false)
+        return homeBinding?.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _fragmentHomeBinding = null
+    }
+
+}
---
+++ app/src/main/java/com/c23ps419/petanikita/ui/scan/ScanFragment.kt
@@ -0,0 +1,29 @@
+package com.c23ps419.petanikita.ui.scan
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.c23ps419.petanikita.databinding.FragmentScanBinding
+
+
+class ScanFragment : Fragment() {
+
+    private var _fragmentScanBinding: FragmentScanBinding? = null
+    private val scanBinding get() = _fragmentScanBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _fragmentScanBinding = FragmentScanBinding.inflate(inflater, container, false)
+        return scanBinding?.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _fragmentScanBinding = null
+    }
+
+}
