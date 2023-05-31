15f88f81ba5683236ec55d2f111cdefb91de0da1
BagaspranawaTB <bagasptbuana@gmail.com>
2023-06-01 03:48:32 +0700

    Remove non existing MainViewModel from ViewModelFactory

--- app/src/main/java/com/c23ps419/petanikita/utils/ViewModelFactory.kt
+++ app/src/main/java/com/c23ps419/petanikita/utils/ViewModelFactory.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.c23ps419.petanikita.di.Injection
 import com.c23ps419.petanikita.ui.login.LoginViewModel
-import com.c23ps419.petanikita.ui.mainhome.MainViewModel
 import com.c23ps419.petanikita.ui.register.RegisterViewModel
 
 @Suppress("UNCHECKED_CAST")
@@ -16,9 +15,6 @@ class ViewModelFactory(): ViewModelProvider.Factory {
             }
             modelClass.isAssignableFrom(RegisterViewModel::class.java) -> {
                 RegisterViewModel(Injection.provideRepository()) as T
-            }
-            modelClass.isAssignableFrom(MainViewModel::class.java) -> {
-                MainViewModel(Injection.provideRepository()) as T
             }
             else -> throw IllegalArgumentException("Unknown ViewModel Class")
         }
