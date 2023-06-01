b8badc41720dd6969436d775ce9d1f5f3c90e5fa
BagaspranawaTB <bagasptbuana@gmail.com>
2023-06-01 16:36:29 +0700

    Adding UserPreferences for local data preference.

---
+++ app/src/main/java/com/c23ps419/petanikita/data/local/datastore/UserPreferences.kt
@@ -0,0 +1,49 @@
+package com.c23ps419.petanikita.data.local.datastore
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class UserPreferences private constructor(private val dataStore: DataStore<Preferences>){
+
+    private object PreferencesKeys {
+        val USER_TOKEN = stringPreferencesKey("user_token")
+        val USER_SESSION = booleanPreferencesKey("user_session")
+    }
+
+    fun getUserSession(): Flow<Boolean> {
+        return dataStore.data.map {preferences ->
+            preferences[PreferencesKeys.USER_SESSION] ?: false
+        }
+    }
+
+    fun getUserToken(): Flow<String> {
+        return dataStore.data.map {preferences ->
+            preferences[PreferencesKeys.USER_TOKEN] ?: ""
+        }
+    }
+
+    suspend fun saveUserLoginData(token: String, session: Boolean){
+        dataStore.edit { preferences ->
+            preferences[PreferencesKeys.USER_TOKEN] = token
+            preferences[PreferencesKeys.USER_SESSION] = session
+        }
+    }
+
+    companion object{
+        @Volatile
+        private var userPreferences: UserPreferences? = null
+
+        fun getInstance(dataStore: DataStore<Preferences>): UserPreferences {
+            return userPreferences ?: synchronized(this){
+                val instance = UserPreferences(dataStore)
+                userPreferences = instance
+                instance
+            }
+        }
+    }
+}
