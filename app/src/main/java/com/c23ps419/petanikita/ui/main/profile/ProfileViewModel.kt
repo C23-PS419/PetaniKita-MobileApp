f1b66eaff8e7b1273421613c72434618de4ad6c9
BagaspranawaTB <bagasptbuana@gmail.com>
2023-06-04 00:57:38 +0700

    Created a ViewModelFactory for the ViewModel that utilizes the database, and also added other injections for it

--- app/src/main/java/com/c23ps419/petanikita/ui/main/profile/ProfileViewModel.kt
+++ app/src/main/java/com/c23ps419/petanikita/ui/main/profile/ProfileViewModel.kt
@@ -1,8 +1,32 @@
 package com.c23ps419.petanikita.ui.main.profile
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
-import com.c23ps419.petanikita.data.DataRepository
+import androidx.lifecycle.viewModelScope
+import com.c23ps419.petanikita.data.repository.UserDatabaseRepository
+import kotlinx.coroutines.launch
 
-class ProfileViewModel(private val dataRepository: DataRepository): ViewModel() {
-    fun userLogout() = dataRepository.getLogout()
+class ProfileViewModel(private val userDatabaseRepository: UserDatabaseRepository): ViewModel() {
+
+    val user = userDatabaseRepository.userData
+
+    init {
+        refreshDataFromRepository()
+    }
+
+    private fun refreshDataFromRepository(){
+        viewModelScope.launch {
+            try {
+                userDatabaseRepository.refreshUserData()
+            } catch (e: Exception){
+                Log.e(TAG,e.message.toString())
+            }
+        }
+    }
+
+    fun userLogout() = userDatabaseRepository.getLogout()
+
+    companion object {
+        const val TAG = "ProfileViewModel"
+    }
 }
