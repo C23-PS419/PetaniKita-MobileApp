2e0df611b94d17283cb00c5f538685c801763815
BagaspranawaTB <bagasptbuana@gmail.com>
2023-06-14 02:24:49 +0700

    Adding validation to the authentication

--- app/src/main/java/com/c23ps419/petanikita/ui/login/LoginActivity.kt
+++ app/src/main/java/com/c23ps419/petanikita/ui/login/LoginActivity.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Patterns
 import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
@@ -65,6 +68,40 @@ class LoginActivity : AppCompatActivity() {
                     }
                 }
             }
+        }
+
+        setMyButtonEnable()
+        loginBinding?.edLoginEmail?.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+            }
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                setMyButtonEnable()
+            }
+            override fun afterTextChanged(s: Editable) {
+            }
+        })
+        loginBinding?.edLoginPassword?.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+            }
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                setMyButtonEnable()
+            }
+            override fun afterTextChanged(s: Editable) {
+            }
+        })
+    }
+
+    private fun setMyButtonEnable(){
+        val email = loginBinding?.edLoginEmail?.text?.toString()
+        val password = loginBinding?.edLoginPassword?.text?.toString()
+        loginBinding?.myButton?.isEnabled = isValidEmail(email) && (password?.length ?: 0) >= 8
+    }
+
+    private fun isValidEmail(email: String?): Boolean {
+        return if (email.isNullOrEmpty()) {
+            false
+        } else {
+            Patterns.EMAIL_ADDRESS.matcher(email).matches()
         }
     }
 
